f2b5592a6bf9b12f3ea7200173b547891c754b38
huoyinghui <huoyinghui@apkpure.com>
2017-07-12 11:33:33 +0800

    route: 链式处理

--- myapp_express/routes/users.js
+++ myapp_express/routes/users.js
@@ -20,4 +20,32 @@ router.get('/:from-:to', function(req, res, next){
 });
 
 
+router.get('/example/a', function(req, res){
+    res.send('hello from a')
+});
+
+router.get('/example/b', function (req, res, next) {
+  console.log('the response will be sent by the next function ...');
+  next();     //调用到hello from B!
+}, function (req, res) {
+  res.send('Hello from B!')
+})
+
+
+var cb0 = function(req, res, next) {
+  console.log('cb0')
+  next()
+}
+
+var cb1 = function(req, res, next) {
+  console.log('cb1')
+  next()
+}
+
+var cb2 = function(req, res) {
+  res.send('Hello from C!')
+}
+
+router.get('/example/c', [cb0, cb1, cb2])
+
 module.exports = router;
