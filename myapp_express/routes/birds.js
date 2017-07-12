d17fea42ba5e48cad1600991755f9be9439686ed
huoyinghui <huoyinghui@apkpure.com>
2017-07-12 11:49:01 +0800

    middlefunc: 测试中间件

--- myapp_express/routes/birds.js
+++ myapp_express/routes/birds.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//测试中间件
+var myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+router.use(myLogger)
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.send('birds home page')
 });
+
 
 module.exports = router;
