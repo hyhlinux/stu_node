d315f5d8430394dc8c6e0ebdbc34312456cc22c0
huoyinghui <huoyinghui@apkpure.com>
2017-07-28 17:17:55 +0800

    test

--- myapp_express/routes/index.js
+++ myapp_express/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var sleep = require('sleep');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/health', function(req, res, next) {
+    console.log('time:', Date.now());
+    sleep.sleep(1);
+    console.log('time:', Date.now());
+    res.render('health', { title: 'health' });
 });
 
 module.exports = router;
